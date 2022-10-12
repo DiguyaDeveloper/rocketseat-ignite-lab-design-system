709dd3b92c5476e7ac842eaf39f8a848fcb3df9e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-12 00:33:44 -0300

    feat(aula-2): :sparkles: Create Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import { HeadingProps } from "./Heading.interface";
+import { Slot } from "@radix-ui/react-slot";
+
+export function Heading({ size = "md", children, asChild }: HeadingProps) {
+  const Tag = asChild ? Slot : "h2";
+  return (
+    <Tag
+      className={clsx("text-gray-100 font-bold font-sans", {
+        "text-lg": size === "sm",
+        "text-xl": size === "md",
+        "text-2xl": size === "lg",
+      })}
+    >
+      {children}
+    </Tag>
+  );
+}
