357975a235275095ec2dd0c8a1b0a94624a405c4
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-13 19:43:17 -0300

    feat(aula-3): :sparkles: Update types in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,14 @@ import clsx from "clsx";
 import { ButtonProps } from "./Button.interface";
 import { Slot } from "@radix-ui/react-slot";
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, ...props }: ButtonProps) {
   const Tag = asChild ? Slot : "button";
   return (
     <Tag
       className={clsx(
         "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
       )}
+      {...props}
     >
       {children}
     </Tag>
