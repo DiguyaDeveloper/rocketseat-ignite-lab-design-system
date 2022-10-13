45438c33882a2ee439214be50e50faa612e637b2
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-13 19:31:22 -0300

    feat(aula-3): :sparkles: Add className property to components

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,15 +2,24 @@ import clsx from "clsx";
 import { HeadingProps } from "./Heading.interface";
 import { Slot } from "@radix-ui/react-slot";
 
-export function Heading({ size = "md", children, asChild }: HeadingProps) {
+export function Heading({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) {
   const Tag = asChild ? Slot : "h2";
   return (
     <Tag
-      className={clsx("text-gray-100 font-bold font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "md",
-        "text-2xl": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "md",
+          "text-2xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Tag>
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,15 +2,19 @@ import clsx from "clsx";
 import { TextProps } from "./Text.interface";
 import { Slot } from "@radix-ui/react-slot";
 
-export function Text({ size = "md", children, asChild }: TextProps) {
+export function Text({ size = "md", children, asChild, className }: TextProps) {
   const Tag = asChild ? Slot : "span";
   return (
     <Tag
-      className={clsx("text-gray-100 font-sans", {
-        "text-xs": size === "sm",
-        "text-md": size === "md",
-        "text-lg": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100 font-sans",
+        {
+          "text-xs": size === "sm",
+          "text-md": size === "md",
+          "text-lg": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Tag>
