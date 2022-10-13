96e723e65f3a44bde0444682d445caf7696f2196
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-13 19:57:53 -0300

    fix(aula-3): :bug: Fix button style input

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,12 +11,10 @@ export function Button({
   const Tag = asChild ? Slot : "button";
   return (
     <Tag
-      className={
-        (clsx(
-          "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
-        ),
-        className)
-      }
+      className={clsx(
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+        className
+      )}
       {...props}
     >
       {children}
